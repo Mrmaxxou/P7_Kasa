6f9c3a18656407928a781f95a53a03fd2d8145df
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-18 12:53:53 +0200

    Ajout couleur tertiary

--- src/utils/style/colors.js
+++ src/utils/style/colors.js
@@ -1,6 +1,7 @@
 const colors = {
     primary: '#FF6060',
     secondary: '#FFFFFF',
+    tertiary: '#000000',
     backgroundCard: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%)'
 }
 
