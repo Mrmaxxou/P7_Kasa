2af463a439519b227b467e71e82217271a3da2c4
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-18 11:19:31 +0200

    Modification logo kasa

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
+        <div>
+            <img src={}
+        
         <nav>
             <Link to="/">Accueil</Link>
             <Link to="/Propos">A Propos</Link>
         </nav>
+        </div>
     )
 }
 
