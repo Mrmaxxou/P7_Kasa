f70a85276bf57e750fbf2918ce677ed7c43979bf
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-20 16:18:05 +0200

    Modification map card et about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,23 +1,23 @@
 import {BannerAbout }from '../../components/Banner'
+import Collapse from '../../components/Collapse'
 
 import styled from 'styled-components'
 
 const StyledAbout = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+ align-items: center;
   margin: 70px 100px 30px 100px;
   @media (max-width: 768px){
     margin: 16px 20px 22px 20px;
   } 
 
 `
-
-
 function About(){
     return (
         <StyledAbout>
             <BannerAbout/>
+            <Collapse/>
         </StyledAbout>
     )
 
